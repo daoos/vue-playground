ed250853a8ff9323ee2d6b196d92c9f95ba1381a
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2017-11-22 23:55:17 -0200

    add first examples

---
+++ src/components/GoBackButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-btn
+      class="ma-3" 
+      outline 
+      small 
+      color="blue darken-1" 
+      title="Voltar"
+      @click="clicked">
+      <v-icon>arrow_back</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+  import { action } from '@storybook/addon-actions';
+
+  export default {
+    name: 'GoBackButton',
+    methods: {
+      clicked () {
+        action('clicked')()
+      }
+    }
+  }
+</script>
